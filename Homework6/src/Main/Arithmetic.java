97545a4cef5d4c17844bab0ef666305d6fc14a5a
cbrbr <cbrbrooks.ruth@gmail.com>
2022-10-22 12:21:38 -0700

    a little revision

--- Homework6/src/Main/Arithmetic.java
+++ Homework6/src/Main/Arithmetic.java
@@ -63,4 +63,5 @@ public class Arithmetic{
 	public static String staticAdd(int value1, int value2){
 		return value1 + " + " + value2 + " = " + (value1+value2);
 	}
+	
 }
--- Homework6/src/Main/Homework6.java
+++ Homework6/src/Main/Homework6.java
@@ -6,21 +6,24 @@ package Main;
 public class Homework6{
 	public static void main(String[] args){
 		  Arithmetic ar;
-		  ar = new Arithmetic(100,5);
-		  System.out.println(ar.toString());
-		  System.out.println(ar.add());
+		 ar = new Arithmetic(100,5);
+		 System.out.println(ar.toString());
+		 System.out.println(ar.add());
 		  System.out.println(ar.subtract());
 		  System.out.println(ar.multiply());
 		  System.out.println(ar.divide());
 		  System.out.println(ar.modulo());
 		  ar.changeValues(5,6);
-		  System.out.println(ar.toString());
+		 System.out.println(ar.toString());
 		  System.out.println(ar.add());
 		  System.out.println(ar.subtract());
 		  System.out.println(ar.multiply());
 		  System.out.println(ar.divide());
 		  System.out.println(ar.modulo());
 		  System.out.println(Arithmetic.staticAdd(12,67));
+		 
+		 
+		
 	}
 	
 	
