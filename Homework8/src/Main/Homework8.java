097fbc96a895846bc0155148b1a05694bd6e1a61
cbrbr <cbrbrooks.ruth@gmail.com>
2022-11-01 11:37:21 -0700

    first attempt at the lab

---
+++ Homework8/src/Main/Homework8.java
@@ -0,0 +1,33 @@
+/* H O M E W O R K   # 8
+ * C S C   1 5
+ * 
+ * Student's Name Here: Ruth Brooks
+ * 
+ * Follow the directions on the Homework #8 hand out and submit this file via Canvas by the due date.
+ * 
+ * */
+
+package Main;
+
+public class Homework8{
+	public static void main(String[] args){
+		// TODO: Follow Homework #8 instructions for assignment and place code below...
+		// Feel free to use the p() method to print text instead of System.out.println()
+		
+		// TODO: Write file data...
+		
+		
+		// TODO: Read file data in and display...
+		
+		
+		// TODO: Append to a file...
+		
+		
+		// TODO: Display results of new file
+		
+	}
+	
+	public static void p(String out){
+		System.out.println(out);
+	}
+}
